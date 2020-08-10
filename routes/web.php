b76448793f791b12b69c2e07bf14040ebf2b7d6d
Tossy Santoso <tossysantos2@gmail.com>
2020-08-10 22:04:54 +0700

    kuis tiga template dan erd

--- routes/web.php
+++ routes/web.php
@@ -12,13 +12,13 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layouts.master');
 });
 
 Route::get('/items/create', 'ItemController@create'); // menampilkan halaman form
 Route::post('/items', 'ItemController@store'); // menyimpan data
 Route::get('/items', 'ItemController@index'); // menampilkan semua
-Route::get('/items/{id}', 'ItemController@show'); // menampilkan detail item dengan id 
+Route::get('/items/{id}', 'ItemController@show'); // menampilkan detail item dengan id
 Route::get('/items/{id}/edit', 'ItemController@edit'); // menampilkan form untuk edit item
 Route::put('/items/{id}', 'ItemController@update'); // menyimpan perubahan dari form edit
 Route::delete('/items/{id}', 'ItemController@destroy'); // menghapus data dengan id
